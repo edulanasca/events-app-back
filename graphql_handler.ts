c8053d816ed1f5c50cd3d0adbcf3836f912c9ebc
Eduardo <edujlac@gmail.com>
2024-08-24 04:56:55 -0500

    update: auth header

--- graphql_handler.ts
+++ graphql_handler.ts
@@ -3,17 +3,17 @@ import schema from './graphql_schema';
 import { prisma } from './prisma';
 import jwt from 'jsonwebtoken';
 import { Context } from './graphql_types';
-import { useCookies } from '@whatwg-node/server-plugin-cookies';
 
 const SECRET_KEY = process.env.SECRET_KEY || 'your_secret_key_here';
 
 const context = async ({ request }: YogaInitialContext): Promise<Context> => {
-    const tokenCookie = await request.cookieStore?.get('auth_token');
+    const authHeader = request.headers.get('Authorization');
     let user = null;
 
-    if (tokenCookie) {
+    if (authHeader && authHeader.startsWith('Bearer ')) {
+        const token = authHeader.split(' ')[1];
         try {
-            const decoded = jwt.verify(tokenCookie.value, SECRET_KEY) as { email: string };
+            const decoded = jwt.verify(token, SECRET_KEY) as { email: string };
             user = await prisma.user.findUnique({
                 where: { email: decoded.email },
             });
@@ -29,6 +29,4 @@ export const yoga = createYoga({
     graphqlEndpoint: "/api/graphql",
     schema,
     context,
-    plugins: [useCookies()]
 });
-
