a6522a360a1e53d3f5fb8d22c1e99cf5704cf446
Eduardo <edujlac@gmail.com>
2024-08-24 01:06:12 -0500

    feat: clusters to use cpu

--- index.ts
+++ index.ts
@@ -4,21 +4,39 @@ import { loginHandler, registerHandler } from './auth_handlers';
 import { yoga } from './graphql_handler';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import cluster from 'cluster';
+import os from 'os';
 
-const app = express();
-const port = process.env.PORT || 3000;
+const numCPUs = os.cpus().length;
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-}));
-app.use(cookieParser());
-app.use(bodyParser.json());
+if (cluster.isPrimary) {
+  console.log(`Master ${process.pid} is running`);
 
-app.post('/api/auth/login', loginHandler);
-app.post('/api/auth/register', registerHandler);
-app.use(yoga.graphqlEndpoint, yoga);
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  cluster.on('exit', (worker) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    cluster.fork(); // Restart the worker
+  });
+} else {
+  const app = express();
+  const port = process.env.PORT || 3000;
+
+  app.use(cors({
+      origin: process.env.FRONTEND_URL,
+      credentials: true,
+  }));
+  app.use(cookieParser());
+  app.use(bodyParser.json());
+
+  app.post('/api/auth/login', loginHandler);
+  app.post('/api/auth/register', registerHandler);
+  app.use(yoga.graphqlEndpoint, yoga);
+
+  app.listen(port, () => {
+    console.log(`Worker ${process.pid} is running on port ${port}`);
+  });
+}
